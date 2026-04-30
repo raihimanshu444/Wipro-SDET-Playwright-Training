624dcd01084a6fc1816cda4a4f0d3a17f7c6febe
Himanshu Rai <raihimanshu7905@gmail.com>
2026-04-30 12:59:12 +0530

    Add JS practice covering string manipulation, arithmetic, and logical operations

---
+++ DAY-1/index.js
@@ -0,0 +1,27 @@
+let a = 5;
+let b = 10;
+
+let str = "Hello, World!";
+
+// String operations
+console.log("String:", str); //Hello, World!
+console.log("Length of string:", str.length); //13
+console.log("Uppercase:", str.toUpperCase()); //HELLO, WORLD!
+console.log("Lowercase:", str.toLowerCase()); //hello, world!
+console.log("Substring (0-5):", str.substring(0, 5)); // Hello
+
+// Mathematical operations
+console.log("Addition:", a + b); // 15
+console.log("Subtraction:", a - b); // -5
+console.log("Multiplication:", a * b); // 50
+console.log("Division:", a / b); // 0.5
+console.log("Remainder:", a % b); // 5
+
+// Logical operations --> boolean
+console.log("a > b:", a > b); // FALSE
+console.log("a < b:", a < b); // TRUE
+console.log("a == b:", a == b); // FALSE (Loose equality)
+console.log("a === b:", a === b); // FALSE (Strict equality - checks value and type)
+console.log("a != b:", a != b); // TRUE
+console.log("a >= b:", a >= b); // FALSE
+console.log("a <= b:", a <= b); // TRUE
