f4aa39bdaf2ce521732e32512d5f10415b43b1a9
Himanshu Rai <raihimanshu7905@gmail.com>
2026-05-12 11:23:04 +0530

    implemented Partial, DeepPartial, Readonly, Omit, and Type Narrowing patterns

---
+++ DAY-10/typegn.ts
@@ -0,0 +1,34 @@
+//Type guard
+
+function format(input: string | number) {
+    if(typeof input === "string") {
+        return input.toUpperCase(); // narrowed 'string'
+    }
+    return input.toFixed(2); // narrowed 'number'
+}
+
+console.log(format("Himanshu"));  // HIMANSHU
+console.log(format(12.2345));     //12.23
+
+interface Bird { fly: () => void, species: string }
+interface Fish { swim: () => void }
+
+function move(animal: Bird | Fish) {
+    if("fly" in animal) {
+        animal.fly(); // narrowed to 'Bird'
+    } else {
+        animal.swim(); // narrowed to 'Fish'
+    }
+}
+
+function isValidBird(bird: Bird): bird is Bird {
+    return bird && typeof bird.species === "string";
+}
+
+const b1 = {fly: () => {}, species: "aerial"}
+if(isValidBird(b1)) {
+    console.log(b1.species); // narrowed to 'Bird'
+}
+
+
+
